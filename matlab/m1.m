8db43bb3148ef375258ead6378ac53881fbe2c16
Ren Wang <iamwrm@gmail.com>
2017-07-07 08:02:25 +0800

    \subsection{Uncertainty of The Distance Measurement}

--- matlab/m1.m
+++ matlab/m1.m
@@ -12,5 +12,11 @@ D =[61.40 61.46 61.20 61.36 61.20 61.50 ];
 D_mean = mean(D);
 D_UR = 0.02 / D_mean * 100;
 
-metalball_m = 1.357 /40 /1000
-density_metalball = 6 * metalball_m /pi /d_mean_m /d_mean_m /d_mean_m  
+metalball_m = 1.357 /40 /1000;
+density_metalball = 6 * metalball_m /pi /d_mean_m /d_mean_m /d_mean_m ;
+
+S= [147.0 147.0 146.5 ];
+S_pingfang = 0;
+for i = [1:3]
+S_pingfang = S_pingfang +power((S(i)-mean(S)),2)
+end
